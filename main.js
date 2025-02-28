9cc37e987991bf85c9329e5bd84e0d4cadb32f7c
CIRI <winciri347@gmail.com>
2025-02-28 11:08:46 +0100

    1 file js con los videos de sam

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log('Tomatela chao nene');
+console.log('y ahora voy a ver si la terminal me imprime todo lo que yo escriba gracias al console.log y a node');
+console.log('parece que si que me lo imprime todo');
